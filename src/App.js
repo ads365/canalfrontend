e80a9bd4e96323291343b7945477568e337f4583
Anmol Singh <anmoldeepsingh365@gmail.com>
2019-05-30 08:56:50 +1000

    fix footer bounce

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import {Layout, Content, Footer} from 'react-mdl';
+import {Layout, Content, Footer, FooterSection, FooterLinkList} from 'react-mdl';
 import Landing from './components/landing';
 import Beta from './components/beta';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
 
 
 
@@ -21,12 +23,15 @@ class App extends Component {
                 </Switch>
               </div>
             </Content>
-            <Footer className="footer">
-              <p>Canal Pty Ltd &copy; 2019</p>
-            </Footer>
+            <footer className="footer">
+              <Grid>
+                <Row className="footer-text">
+                  <p>Canal Pty Ltd &copy; 2019</p>
+                </Row>
+              </Grid>
+            </footer>
           </Layout>
         </div>
-
       </BrowserRouter>
     );
   }
